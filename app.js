11c53e499b5b28fd942a0c87b119fc1357e2f6c3
deirdreams <deirdrebringas@gmail.com>
2018-03-10 17:27:17 +0000

    not complete - comment out code to compile

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const	app			= require('express'),
 		server		= require('http').Server(app),
 		io 			= require('socket.io')(server);
 
+var		lobbies		= [],
+		numPlayers	= 0;
+
 server.listen(80);
 
 app.get('/', function (req, res) {
@@ -9,8 +12,33 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
+	//send event to everyone
+  // socket.emit('news', { hello: 'world' });
+
+  //same event name as client
+  socket.on('jump', function (data) {
+  	//send to all clients (including sender) - user broadcast.emit to exclude sender
+  	socket.emit('jumpEvent', "Player jumped")
     console.log(data);
   });
+
+  socket.on('disconnect', function () {
+    io.emit('user disconnected');
+  });
 });
+
+
+//Generating 5-digit ids
+function makeid() {
+  var id = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 5; i++)
+    id += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  if (lobbies.includes(id)) {
+  	return makeid();
+  } else {
+  	return id;
+  }
+}
